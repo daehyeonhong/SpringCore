636c55d27612b93cf61b010edd421d1cc9139b69
HonGDae <fholng@outlook.com>
2022-01-19 22:56:21 +0900

    [Order] Test

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        final MemberService memberService = new MemberServiceImpl();
+        final OrderService orderService = new OrderServiceImpl();
+
+        final Long memberId = 1L;
+        final Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        final Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        System.out.println("order = " + order);
+        System.out.println("calculatePrice = " + order.calculatePrice());
+    }
+}
---
+++ src/test/java/hello/core/order/OrderServiceTests.java
@@ -0,0 +1,25 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTests {
+
+    private final MemberService memberService = new MemberServiceImpl();
+    private final OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        final Long memberId = 1L;
+        final Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        final Order order = orderService.createOrder(memberId, "itemA", 10000);
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
