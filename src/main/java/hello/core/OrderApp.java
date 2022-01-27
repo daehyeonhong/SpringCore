1e8f024ecfb50975f40bbe18726722104509df34
HonGDae <fholng@outlook.com>
2022-01-27 22:31:02 +0900

    mkdir bintest

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -5,12 +5,15 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        final MemberService memberService = appConfig.memberService();
-        final OrderService orderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        final MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        final OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         final Long memberId = 1L;
         final Member member = new Member(memberId, "memberA", Grade.VIP);
