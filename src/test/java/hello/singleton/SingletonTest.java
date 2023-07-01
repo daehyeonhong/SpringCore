bc6c2936ab361df4a79997b3eb173c04eb527733
HongDaeHyeon <fholng@outlook.com>
2023-07-01 12:13:30 +0900

    Singleton이 아닌 DIContainer의 문제점

---
+++ src/test/java/hello/singleton/SingletonTest.java
@@ -0,0 +1,26 @@
+package hello.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SingletonTest {
+    @Test
+    @DisplayName("`Spring`이 없는 순수한 DI Continer")
+    void pureContainer() {
+        final AppConfig appConfig = new AppConfig();
+        final MemberService memberService1 = appConfig.memberService();
+        final MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1)
+                .isNotSameAs(memberService2)
+                .isNotEqualTo(memberService2)
+                .isInstanceOf(MemberService.class);
+    }
+}
